6d852caefb0fa28c146bc3d179c1a6bec331cf1f
Caroline Noronha <carolnte@gmail.com>
2021-04-19 13:16:50 -0300

    First commit

--- apps/banking_api/lib/banking_api.ex
+++ apps/banking_api/lib/banking_api.ex
@@ -37,4 +37,55 @@ defmodule BankingApi do
 
   end
 
+  def transfer(account_id, recipient, value) do
+    #Selecionar a conta remetente
+    #Selecionar a conta destinatária
+    #Checar se ambas existem
+    #Selecionar o valor
+    #Subtrair o valor da conta remetente (se possível)
+    #Adicionar à conta destino
+
+    Repo.transaction(fn ->
+
+      query1 = from a in BankingApi.Accounts.Schemas.Account, where: a.id == ^account_id , lock: "FOR UPDATE"
+
+      account1 = Repo.one(query1)
+      if not is_nil(account1) do
+      Account.changeset(account1, %{balance: account1.balance - value})
+      |> Repo.update()
+      |> case do
+        {:ok, _} -> :ok
+        {:error, changeset} -> Repo.rollback(changeset)
+      end
+      else
+      Repo.rollback(:account_not_found)
+      end
+
+
+      query2 = from a in BankingApi.Accounts.Schemas.Account, where: a.id == ^recipient , lock: "FOR UPDATE"
+
+      account2 = Repo.one(query2)
+      if not is_nil(account2) do
+      Account.changeset(account2, %{balance: account2.balance + value})
+      |> Repo.update()
+      |> case do
+        {:ok, _} -> :ok
+        {:error, changeset} -> Repo.rollback(changeset)
+      end
+      else
+      Repo.rollback(:account_not_found)
+      end
+
+
+      Operation.changeset(%{type: "Transfer", value: value, sender: account1.id})
+      |> Repo.insert()
+      |> case do
+        {:ok, _} -> :ok
+        {:error, changeset} -> Repo.rollback(changeset)
+      end
+
+    end)
+
+  end
+
 end
