98d96848b0a2282681fad9886c4823a0c4399f4d
Caroline Noronha <carolnte@gmail.com>
2021-04-20 09:07:17 -0300

    Deposit variables name set

--- apps/banking_api/lib/banking_api.ex
+++ apps/banking_api/lib/banking_api.ex
@@ -7,6 +7,8 @@ defmodule BankingApi do
 
   @assets_account_id "47616e06-e26e-46a9-a9e4-ed5d04d4b129"
 
+  #Switch variables name for pattern
+
   def withdraw(source_account_id, value) do
     Repo.transaction(fn ->
       query =
@@ -59,19 +61,18 @@ defmodule BankingApi do
     end)
   end
 
-  # Change variables name
 
   def deposit(source_account_id, value) do
     Repo.transaction(fn ->
-      query =
+      deposit_query =
         from a in BankingApi.Accounts.Schemas.Account,
           where: a.id == ^source_account_id,
           lock: "FOR UPDATE"
 
-      account = Repo.one(query)
+      deposit_account = Repo.one(deposit_query)
 
-      if not is_nil(account) do
-        Account.changeset(account, %{balance: account.balance + value})
+      if not is_nil(deposit_account) do
+        Account.changeset(deposit_account, %{balance: deposit_account.balance + value})
         |> Repo.update()
         |> case do
           {:ok, _} -> :ok
@@ -81,15 +82,15 @@ defmodule BankingApi do
         Repo.rollback(:account_not_found)
       end
 
-      query_asset =
+      deposit_query_asset =
         from a in BankingApi.Accounts.Schemas.Account,
           where: a.id == ^@assets_account_id,
           lock: "FOR UPDATE"
 
-      account_asset = Repo.one(query_asset)
+      deposit_account_asset = Repo.one(deposit_query_asset)
 
-      if not is_nil(account_asset) do
-        Account.changeset(account_asset, %{balance: account_asset.balance - value})
+      if not is_nil(deposit_account_asset) do
+        Account.changeset(deposit_account_asset, %{balance: deposit_account_asset.balance - value})
         |> Repo.update()
         |> case do
           {:ok, _} -> :ok
@@ -102,7 +103,7 @@ defmodule BankingApi do
       Operation.changeset(%{
         type: "Deposit",
         value: value,
-        target_account_id: account.id,
+        target_account_id: deposit_account.id,
         source_account_id: @assets_account_id
       })
       |> Repo.insert()
